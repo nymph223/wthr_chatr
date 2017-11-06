45abc678efb5f519af81f2875c7469b9b6e56766
ChanYub.Park <mrchypark@gmail.com>
2017-11-06 23:48:59 +0900

    set keyboard

--- app.R
+++ app.R
@@ -8,5 +8,11 @@ jug() %>%
   get("/", function(req, res, err){
     "Hello World!"
   }) %>%
+  get("/keyboard", function(req,res,err){
+    body<-list(type="buttons",
+               buttons= c("선택1","선택2","선택3"))
+    res$json(body)
+    res$set_header("Content-Type", "application/json; charset=utf-8")
+  }) %>% 
   simple_error_handler_json() %>%
   serve_it()
